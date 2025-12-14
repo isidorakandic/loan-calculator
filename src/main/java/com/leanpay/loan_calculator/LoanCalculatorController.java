44e2fca7892984d56e6871076e40d99eeafbda57
Isidora Kandić <isidora.kandic@gmail.com>
2025-12-14 13:36:21 +0100

    mysql connection setup

--- src/main/java/com/leanpay/loan_calculator/LoanCalculatorController.java
+++ src/main/java/com/leanpay/loan_calculator/LoanCalculatorController.java
@@ -2,12 +2,10 @@ package com.leanpay.loan_calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 class LoanCalculatorController {
-
-
+    
     @GetMapping("/hello")
     public String sayHi() {
         return "Hello Spring Boot world!";
