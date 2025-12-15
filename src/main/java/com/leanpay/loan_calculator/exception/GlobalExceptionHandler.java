42c3340c2fe6200d304f0b304005b35be546f975
Isidora Kandić <isidora.kandic@gmail.com>
2025-12-15 21:52:46 +0100

    error logging added

--- src/main/java/com/leanpay/loan_calculator/exception/GlobalExceptionHandler.java
+++ src/main/java/com/leanpay/loan_calculator/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.leanpay.loan_calculator.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -14,10 +16,14 @@ import java.util.List;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException exception) {
         List<String> errors = extractMessages(exception);
+        log.warn("Parameter validation failed: {}", errors, exception);
         ErrorResponse response = new ErrorResponse(errors);
         return ResponseEntity.badRequest().body(response);
     }
@@ -26,17 +32,19 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleJsonParseError(HttpMessageNotReadableException exception) {
         String message = "Malformed JSON or invalid parameter data type was sent.";
+        log.warn(message, exception);
         return new ErrorResponse(List.of(message));
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleUnexpected(Exception exception) {
+        log.error("Unexpected error occurred", exception);
         return new ErrorResponse(List.of(
                 "An internal server error occurred. We're working on fixing it."
         ));
     }
-    
+
     private static List<String> extractMessages(MethodArgumentNotValidException exception) {
         return exception.getBindingResult()
                 .getAllErrors()
