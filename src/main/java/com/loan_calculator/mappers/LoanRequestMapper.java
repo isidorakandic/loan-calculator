f20513dd394381ce2e88c6d0e1464c6305fdea89
Isidora Kandić <isidora.kandic@gmail.com>
2025-12-16 20:37:47 +0100

    remove after mapping rounding (service now does that)

--- src/main/java/com/loan_calculator/mappers/LoanRequestMapper.java
+++ src/main/java/com/loan_calculator/mappers/LoanRequestMapper.java
@@ -5,13 +5,9 @@ import com.loan_calculator.dto.InstallmentDTO;
 import com.loan_calculator.dto.LoanResponseDTO;
 import com.loan_calculator.entity.Installment;
 import com.loan_calculator.entity.LoanRequest;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
@@ -28,17 +24,5 @@ public interface LoanRequestMapper {
 
     @IterableMapping(elementTargetType = LoanResponseDTO.class)
     List<LoanResponseDTO> toLoanResponseDTOs(List<LoanRequest> loanRequests);
-
-    @AfterMapping
-    default void roundDto(@MappingTarget InstallmentDTO dto) {
-        dto.setPrincipalAmount(roundToTwoDecimals(dto.getPrincipalAmount()));
-        dto.setInterestAmount(roundToTwoDecimals(dto.getInterestAmount()));
-        dto.setBalanceOwed(roundToTwoDecimals(dto.getBalanceOwed()));
-        dto.setPaymentAmount(roundToTwoDecimals(dto.getPaymentAmount()));
-    }
-
-    private BigDecimal roundToTwoDecimals(BigDecimal value) {
-        if (value == null) return null;
-        return value.setScale(2, RoundingMode.HALF_UP);
-    }
+    
 }
