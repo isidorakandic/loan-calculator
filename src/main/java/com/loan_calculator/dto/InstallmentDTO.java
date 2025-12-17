c746818e5403fc4fdcade56335404aa2228ffc84
Isidora Kandić <isidora.kandic@gmail.com>
2025-12-17 16:35:16 +0100

    cosmetic change

--- src/main/java/com/loan_calculator/dto/InstallmentDTO.java
+++ src/main/java/com/loan_calculator/dto/InstallmentDTO.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor // for MapStruct (Installment -> InstallmentDTO)
 @Setter  // for MapStruct
 @Getter // for Jackson (InstallmentDTO -> JSON)
-@EqualsAndHashCode // for integration testing
+@EqualsAndHashCode // for unit testing
 public class InstallmentDTO {
 
     private int month;
