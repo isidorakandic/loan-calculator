2285f348cb03bc5ab9e8d7173af27175c7a2f05f
Isidora Kandić <isidora.kandic@gmail.com>
2026-01-04 15:06:21 +0100

    moved test data for calculation UTs to json file

--- src/test/java/com/loan_calculator/service/LoanCalculationTest.java
+++ src/test/java/com/loan_calculator/service/LoanCalculationTest.java
@@ -1,8 +1,8 @@
 package com.loan_calculator.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.loan_calculator.entity.Installment;
 import com.loan_calculator.entity.LoanRequest;
 import org.junit.jupiter.api.Named;
@@ -23,8 +23,8 @@ class LoanCalculationTest {
 
     private final LoanCalculationImpl loanCalculation = new LoanCalculationImpl();
 
-    @ParameterizedTest(name = "{0}")
-    @MethodSource("loanRequestProvider")
+    @ParameterizedTest(name = "{0}") // {0} is a placeholder for the 1. argument that's passed to the method
+    @MethodSource("testCasesArgumentsProvider")
     void calculateInstallments(LoanRequest loanRequest, List<Installment> expectedInstallments) {
         List<Installment> actualInstallments = loanCalculation.calculateInstallments(loanRequest);
         List<Installment> actualInstallmentToBeChecked =
@@ -32,34 +32,22 @@ class LoanCalculationTest {
         assertInstallmentsMatchExpected(actualInstallmentToBeChecked, expectedInstallments);
     }
 
-    private static Stream<Arguments> loanRequestProvider() throws IOException {
+    private static Stream<Arguments> testCasesArgumentsProvider() throws IOException {
         InputStream inputStream = LoanCalculationTest.class.getResourceAsStream("/loan-cases.json");
-        if (inputStream == null) {
-            throw new IllegalStateException("loan-cases.json not found");
-        }
-        try (inputStream) {
-            JsonMapper jsonMapper = JsonMapper.builder().build();
-            JsonNode rootNode = jsonMapper.readTree(inputStream);
+        JsonMapper mapper = JsonMapper.builder()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .build();
 
-            if (rootNode == null || !rootNode.isArray()) {
-                throw new IllegalStateException("loan-cases.json is not an array");
-            }
+        // we map to ArrayNode instead of mapping directly ro LoanRequest because we want to read 'name' property as well
+        ArrayNode nodes = (ArrayNode) mapper.readTree(inputStream);
 
-            return StreamSupport.stream(rootNode.spliterator(), false).map(node -> {
-                ObjectNode loanRequestNode = node.deepCopy();
-                String name = loanRequestNode.path("name").asText();
-                if (name == null || name.isBlank()) {
-                    throw new IllegalStateException("Test case name is missing in loan-cases.json");
-                }
-                loanRequestNode.remove("name");
-
-                LoanRequest loanRequest = jsonMapper.convertValue(loanRequestNode, LoanRequest.class);
-
-                List<Installment> expectedInstallments = new ArrayList<>(loanRequest.getInstallments());
-                loanRequest.setInstallments(new ArrayList<>());
-                return Arguments.of(Named.of(name, loanRequest), expectedInstallments);
-            });
-        }
+        return StreamSupport.stream(nodes.spliterator(), false)
+                .map(node -> {
+                    LoanRequest loanRequest = mapper.convertValue(node, LoanRequest.class);
+                    List<Installment> expectedInstallments = new ArrayList<>(loanRequest.getInstallments());
+                    loanRequest.setInstallments(new ArrayList<>());
+                    return Arguments.of(Named.of(node.get("name").asText(), loanRequest), expectedInstallments);
+                });
     }
 
 
